b7b9caa76f907a828c228e4772170a7b461e8ee1
Tero <tero.pau@gmail.com>
2019-01-08 14:12:55 +0200

    initialized CatProgram. Program contains cats

---
+++ CatProgram/CatProgram/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatProgram
+{
+    public class Cat
+    {
+        public string name;
+        public int weight;
+        public Cat()
+        {
+            this.weight = 1;
+            //Console.WriteLine($"Kissan paino on {this.weight}");
+        }
+        public Cat(string name, int weightValue)
+        {
+            this.name = name;
+            this.weight = weightValue;
+        }
+        public Cat(int weightValue)
+        {
+            this.weight = weightValue;
+            //Console.WriteLine($"Kissan paino on {this.weight}");
+        }
+        public void PrintWeight()
+        {
+            Console.WriteLine($"Kissan paino on {this.weight}");
+        }
+    }
+}
---
+++ CatProgram/CatProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CatProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a cat program.");
+            Cat myFirstCat = new Cat("Mr.Nancy", 1);
+            myFirstCat.weight = 5;
+            Console.WriteLine($"Kissaa on syötetty. Uusi painoon. {myFirstCat.weight}");
+            Cat mySecondCat = new Cat("Mittens", 3);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
