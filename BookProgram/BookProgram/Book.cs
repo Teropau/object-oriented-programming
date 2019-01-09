11f17edfda1774bee4acb54b4010f9b13dffa046
Tero <tero.pau@gmail.com>
2019-01-09 10:27:30 +0200

    Program creates and compares books

---
+++ BookProgram/BookProgram/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookProgram
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public decimal price;
+        public Book(string title, string author, string id, decimal price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+        public string PrintInfo()
+        {
+            return $"Nimi: {this.title} \nKirjailija: {this.author} \nid: {this.id} \nHinta: {this.price} \n";
+        }
+        public string BookCompare(Book book)
+        {
+            string text = "";
+            if (this.price > book.price)
+            {
+                 text = $"Kirja {this.title} on kalliimpi, kuin {book.title}";
+            }
+            else if(this.price == book.price)
+            {
+                 text = $"Kirja {this.title} on samanhintainen, kuin {book.title}";
+            }
+            else
+            {
+                 text = $"Kirja {book.title} on kalliimpi, kuin {this.title}";
+            }
+            return text;
+        }
+    }
+}
---
+++ BookProgram/BookProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjaohjelma");
+            Book newBook = new Book("How to cut Jew", "Master Carpenter", "00001", 24);
+            Console.WriteLine(newBook.PrintInfo());
+            Book newBook2 = new Book("Aapinen", "Agricola", "00002", 12);
+            Console.WriteLine(newBook2.PrintInfo());
+            Console.WriteLine("------------------");
+            Console.WriteLine(newBook.BookCompare(newBook2));
+        }
+
+    }
+}
