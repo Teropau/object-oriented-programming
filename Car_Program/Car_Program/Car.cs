6b2b91374131fbd9ba3fbf3b68cd794f4c7f0349
Tero <tero.pau@gmail.com>
2019-01-14 10:00:06 +0200

    Program races cars

---
+++ Car_Program/Car_Program/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_Program
+{
+    class Car
+    {
+        string brand;
+        double speed;
+        public Car()
+        {
+            this.brand = "";
+            this.speed = 0;
+        }
+        public void AskData()
+        {
+            Console.WriteLine("Syötä auton merkki (Ei mikään halpa romu kiitos):");
+            this.brand = Console.ReadLine();
+            Console.WriteLine($"Syötä auton {this.brand} nopeus (km/h):");
+            this.speed = int.Parse(Console.ReadLine());
+
+        }
+        public string ShowCarInfo()
+        {
+            return $"Merkki: {this.brand} Nopeus: {this.speed} km/h";
+        }
+        public void Accelerate()
+        {
+            int gas = 0;
+            do
+            {
+                Console.WriteLine($"Syötä vauhdin lisäys autolle {this.brand}:");
+                gas = int.Parse(Console.ReadLine());
+            } while (gas < 0);
+            this.speed += gas;
+        }
+        public void Brake()
+        {
+            this.speed = this.speed * 0.9;
+        }
+
+    }
+}
---
+++ Car_Program/Car_Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Car_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ralli");
+            Car car1 = new Car();
+            Car car2 = new Car();
+
+            car1.AskData();
+            car2.AskData();
+
+            Console.WriteLine(car1.ShowCarInfo());
+            Console.WriteLine(car2.ShowCarInfo());
+
+            car1.Accelerate();
+            car2.Accelerate();
+
+            Console.WriteLine(car1.ShowCarInfo());
+            Console.WriteLine(car2.ShowCarInfo());
+            Console.WriteLine("Jarrutus");
+            car1.Brake();
+            car2.Brake();
+
+            Console.WriteLine(car1.ShowCarInfo());
+            Console.WriteLine(car2.ShowCarInfo());
+        }
+    }
+}
