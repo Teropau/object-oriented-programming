b5f673682bd009f9bb7777cdb58d51f8d531ee7a
Tero <tero.pau@gmail.com>
2019-01-18 08:44:26 +0200

    Program compares two salaries

---
+++ Employee_Class_Program/Employee_Class_Program/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Class_Program
+{
+    class Employee
+    {
+        public string name;
+        public int id = 0;
+        public string position;
+        public double salary;
+
+        /// <summary>
+        /// name, id, position, salary
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="id"></param>
+        /// <param name="position"></param>
+        /// <param name="salary"></param>
+        public Employee()
+        {
+            this.name = "Ei nimeä";
+            this.id = 0;
+            this.position = "Ei tehtävää";
+            this.salary = 0;
+        }
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+        public void PrintEmployeeInfo()
+        {
+
+            Console.WriteLine($"Nimi:{this.name}, Id:{this.id}, Tehtävä:{this.position}, Palkka:{this.salary:f}e");
+        }
+        public void CompareSalary(Employee person)
+        {
+            if (this.salary > person.salary)
+            {
+                Console.WriteLine($"Duunarin: {this.name} palkka: {this.salary} on suurempi, kuin duunarin:{person.name} palkka: {person.salary}.");
+            }
+            else if (this.salary < person.salary)
+            {
+                Console.WriteLine($"Duunarin: {this.name} palkka: {this.salary} on pienempi, kuin duunarin:{person.name} palkka: {person.salary}.");
+            }
+            else
+            {
+                Console.WriteLine($"Duunarin: {this.name} palkka: {this.salary} on sama, kuin duunarin:{person.name} palkka: {person.salary}.");
+            }
+        }
+    }
+}
---
+++ Employee_Class_Program/Employee_Class_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Employee_Class_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+
+            employees[0] = new Employee ("J.Järvinen", 1, "Rakennuskosmetoloogi", 5000);
+            employees[1] = new Employee("Mikko", 2, "Rakeennusmiäs", 2200);
+            employees[2] = new Employee("Timo", 3, "Ragemaalari", 2400);
+            foreach (Employee person in employees)
+            {
+                person.PrintEmployeeInfo();
+            }
+            employees[0].CompareSalary(employees[1]);
+        }
+    }
+}
