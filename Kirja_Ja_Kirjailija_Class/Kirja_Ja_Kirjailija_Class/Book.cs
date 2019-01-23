9960cb00643b0ca99820db779c286db3551e10f7
Tero <tero.pau@gmail.com>
2019-01-23 13:31:07 +0200

    new program.

---
+++ Kirja_Ja_Kirjailija_Class/Kirja_Ja_Kirjailija_Class/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirja_Ja_Kirjailija_Class
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        public string publisher;
+        public double price;
+        public static string theme;
+
+        /// <summary>
+        /// nimi, kirjailija, Kustantaja, hinta
+        /// </summary>
+        public Book()
+        {
+            this.name = "";
+            this.author = "";
+            this.publisher = "";
+            this.price = 0;
+        }
+
+        public Book(string name, string author, string publisher, double price)
+        {
+            this.name = name;
+            this.author = author;
+            this.publisher = publisher;
+            this.price = price;
+        }
+        /// <summary>
+        /// nimi
+        /// </summary>
+        /// <param name="name"></param>
+        public void FetchBook(string name)
+        {
+            if (this.name == name)
+            {
+                Console.WriteLine($"Nimi:{this.name}\n" +
+                    $"Kirjailija:{this.author}\n" +
+                    $"Kustantaja:{this.publisher}\n" +
+                    $"Hinta:{this.price:C}\n" +
+                    $"Teema:{theme}");
+            }
+            else
+            {
+                Console.WriteLine("Kirjaa ei löytynyt.");
+            }
+        }
+
+        public void SwitchTheme()
+        {
+            Console.Write("Syötä uusi teema:");
+            theme = Console.ReadLine();
+        }
+
+        public double Price
+        {
+            get { return price; }
+
+            set
+            {
+                if (price > 30)
+                {
+                    Console.WriteLine($"Uusi hinta on: {price * 0.9:C}");
+                }
+                    
+            }
+        }
+    }
+}
---
+++ Kirja_Ja_Kirjailija_Class/Kirja_Ja_Kirjailija_Class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kirja_Ja_Kirjailija_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Kirja ja kirjailija");
+            Book kirja = new Book("Twelve rules for life, an anditote for chaos", "Jordan Peterson", "Penguin Random House", 40.00);
+            Book lehti = new Book("The subtle art of not giving a f*ck", "Mark Manson", "Atena", 14.90);
+            kirja.FetchBook("Twelve rules for life, an anditote for chaos");
+            kirja.Price = kirja.price;
+        }
+    }
+}
