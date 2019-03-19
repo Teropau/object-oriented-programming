256e97e928a05fa2f6fe565be9bee896f228f123
Tero <tero.pau@gmail.com>
2019-03-19 17:15:04 +0200

    initialized

---
+++ MPHarjoitustyö/MPHarjoitustyö/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MPHarjoitustyö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
