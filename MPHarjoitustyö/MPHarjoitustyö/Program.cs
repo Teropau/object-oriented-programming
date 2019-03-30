f4c7fd10c7c9b47831218e52fe47d0a379e7a52c
Tero <tero.pau@gmail.com>
2019-03-30 20:25:59 +0200

    Movielist Done

--- MPHarjoitustyö/MPHarjoitustyö/Program.cs
+++ MPHarjoitustyö/MPHarjoitustyö/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
 namespace MPHarjoitustyö
-{
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Elokuvaluettelo");
+            Console.WriteLine("Valitse toiminto\n" +
+                "1: Tulosta koko lista \n" +
+                "2: Tulosta lista Tyylilajin mukaan.\n" +
+                "3: Tulosta tiettynä vuonna julkaistut elokuvat.");
+            FileManager fm = new FileManager("lol");
+
+            int caseSwitch = int.Parse(Console.ReadLine());
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Tulostetaan koko lista.");
+                    fm.PrintAll();
+                    Console.WriteLine("Syötä Elokuvan nimi saadaksesi kaikki tiedot.");
+                    fm.PrintMovieInfo();
+                    break;
+                case 2:
+                    Console.WriteLine("Tulostetaan tyylilajin mukaan");
+                    Console.Write("Kirjoita haettava tyylilaji:");
+                    fm.PrintMoviesByGenre();
+                    Console.WriteLine("Syötä Elokuvan nimi saadaksesi kaikki tiedot.");
+                    fm.PrintMovieInfo();
+                    break;
+                case 3:
+                    Console.WriteLine("Tulostetaan julkaisuvuoden mukaan.");
+                    Console.Write("Kirjoita haettava vuosi:");
+                    fm.SearchByYear();
+                    Console.WriteLine("Syötä Elokuvan nimi saadaksesi kaikki tiedot.");
+                    fm.PrintMovieInfo();
+                    break;
+            }
         }
     }
 }
